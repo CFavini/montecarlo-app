9b8e76cf7eb2692f006f7e536903fead4c4a5425
CFavini <cefavini@gmail.com>
2025-05-15 14:43:23 -0300

    feat: Secrets

--- backend/config.py
+++ backend/config.py
@@ -1,17 +1,18 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'changeme')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+'''
+class Config:
+
     SECRET_KEY = os.getenv('SECRET_KEY', 'change_me')
-    '''
+
     SQLALCHEMY_DATABASE_URI = \
         'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-
     SQLALCHEMY_DATABASE_URI = 'sqlite:////mnt/data/app.db'
-    '''
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+'''
+
